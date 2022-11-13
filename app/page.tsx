fb07a85af5a95155eeb1869df8e42e4defb1194e
starwalker00 <aaronwalkerup@gmail.com>
2022-11-13 19:57:12 +0100

    feat(style): setup tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,31 @@
 import Image from 'next/image'
-import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
+    <div>
+      <main className="flex-col py-12 border justify-center">
+        <h1 className="text-center text-3xl font-bold underline">
+          Hello, Next.js!
         </h1>
 
-        <p className={styles.description}>
+        <p className="text-center py-2">
           Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
+          <code >app/page.tsx</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+        <div className="flex flex-col sm:flex-row border p-8 justify-center">
+          <a href="https://beta.nextjs.org/docs"
+            className="text-center border rounded-xl shadow-lg p-8 space-y-4"
+          >
+            <h2 className="text-center text-xl font-bold">Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js 13</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            className="text-center border rounded-xl shadow-lg p-8 space-y-4"
           >
-            <h2>Examples &rarr;</h2>
+            <h2 className="text-center text-xl font-bold">Examples &rarr;</h2>
             <p>Explore the Next.js 13 playground.</p>
           </a>
 
@@ -32,22 +33,23 @@ export default function Home() {
             href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-            className={styles.card}
+            className="text-center border rounded-xl shadow-lg p-8 space-y-4"
           >
-            <h2>Deploy &rarr;</h2>
+            <h2 className="text-center text-xl font-bold">Deploy &rarr;</h2>
             <p>Deploy your Next.js site to a public URL with Vercel.</p>
           </a>
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      <footer className="min-w-full flex p-4 rounded-lg shadow">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
+          className="mx-auto border"
         >
           Powered by{' '}
-          <span className={styles.logo}>
+          <span>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
