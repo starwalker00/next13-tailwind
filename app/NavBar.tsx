3971c3ab1304dcd82ebc078602b036eb177c9e21
starwalker00 <aaronwalkerup@gmail.com>
2022-11-21 21:56:07 +0100

    feat: use next/link for nav

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,12 +3,13 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
 
 const navigation = [
-    { name: 'Dashboard', href: '#', current: true },
-    { name: 'Team', href: '#', current: false },
-    { name: 'Projects', href: '#', current: false },
-    { name: 'Calendar', href: '#', current: false },
+    { name: 'Dashboard', href: '/', current: true },
+    { name: 'Explore', href: '/explore', current: false },
+    { name: 'Projects', href: '/', current: false },
+    { name: 'Calendar', href: '/', current: false },
 ]
 
 function classNames(...classes: any) {
@@ -49,7 +50,7 @@ export default function NavBar() {
                                 <div className="hidden sm:ml-6 sm:block">
                                     <div className="flex space-x-4">
                                         {navigation.map((item) => (
-                                            <a
+                                            <Link
                                                 key={item.name}
                                                 href={item.href}
                                                 className={classNames(
@@ -59,7 +60,7 @@ export default function NavBar() {
                                                 aria-current={item.current ? 'page' : undefined}
                                             >
                                                 {item.name}
-                                            </a>
+                                            </Link>
                                         ))}
                                     </div>
                                 </div>
@@ -97,32 +98,32 @@ export default function NavBar() {
                                         <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                                             <Menu.Item>
                                                 {({ active }) => (
-                                                    <a
+                                                    <Link
                                                         href="#"
                                                         className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                     >
                                                         Your Profile
-                                                    </a>
+                                                    </Link>
                                                 )}
                                             </Menu.Item>
                                             <Menu.Item>
                                                 {({ active }) => (
-                                                    <a
+                                                    <Link
                                                         href="#"
                                                         className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                     >
                                                         Settings
-                                                    </a>
+                                                    </Link>
                                                 )}
                                             </Menu.Item>
                                             <Menu.Item>
                                                 {({ active }) => (
-                                                    <a
+                                                    <Link
                                                         href="#"
                                                         className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
                                                     >
                                                         Sign out
-                                                    </a>
+                                                    </Link>
                                                 )}
                                             </Menu.Item>
                                         </Menu.Items>
@@ -137,7 +138,7 @@ export default function NavBar() {
                             {navigation.map((item) => (
                                 <Disclosure.Button
                                     key={item.name}
-                                    as="a"
+                                    as={Link}
                                     href={item.href}
                                     className={classNames(
                                         item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
---
+++ app/explore/page.tsx
@@ -0,0 +1,43 @@
+export default function Explore() {
+    return (
+        <div className="grow flex flex-col justify-center items-center">
+            <main className="pt-[10%] flex-grow bg-indigo-100">
+                <h1 className="text-center text-3xl font-bold underline">
+                    Explore
+                </h1>
+
+                <p className="text-center py-2">
+                    Get started by editing{' '}
+                    <code>app/page.tsx</code>
+                </p>
+
+                <div className="flex flex-col sm:flex-row p-8 justify-center gap-4">
+                    <a href="https://beta.nextjs.org/docs"
+                        className="text-center border rounded-xl shadow-lg p-8 space-y-4"
+                    >
+                        <h2 className="text-center text-xl font-bold">Documentation &rarr;</h2>
+                        <p>Find in-depth information about Next.js 13</p>
+                    </a>
+
+                    <a
+                        href="https://github.com/vercel/next.js/tree/canary/examples"
+                        className="text-center border rounded-xl shadow-lg p-8 space-y-4"
+                    >
+                        <h2 className="text-center text-xl font-bold">Examples &rarr;</h2>
+                        <p>Explore the Next.js 13 playground.</p>
+                    </a>
+
+                    <a
+                        href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-center border rounded-xl shadow-lg p-8 space-y-4"
+                    >
+                        <h2 className="text-center text-xl font-bold">Deploy &rarr;</h2>
+                        <p>Deploy your Next.js site to a public URL with Vercel.</p>
+                    </a>
+                </div>
+            </main>
+        </div>
+    )
+}
